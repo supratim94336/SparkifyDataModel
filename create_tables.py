05c114c6dd1189abfb9eb5f49bb403feec1312d8
Supratim <supratim.gre2014@yahoo.com>
2019-05-13 18:59:01 +0200

    files changed for second submission

--- create_tables.py
+++ create_tables.py
@@ -8,19 +8,22 @@ def create_database():
     :return:
     """
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb "
+                            "user=student password=student")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
 
     # create sparkify database with UTF8 encoding
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' "
+                "TEMPLATE template0")
 
     # close connection to default database
     conn.close()
 
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb "
+                            "user=student password=student")
     cur = conn.cursor()
 
     return cur, conn
--- sql_queries.py
+++ sql_queries.py
@@ -7,58 +7,87 @@ artist_table_drop = "DROP TABLE IF EXISTS artists;"
 time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
-# There is nothing about the songplay_id, so it is assigned a serially incremented integer
+
 songplay_table_create = ("""
- CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL, start_time timestamp, user_id varchar, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);
+ CREATE TABLE IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, 
+ start_time timestamp, user_id varchar NOT NULL, level varchar NOT NULL, 
+ song_id varchar, artist_id varchar, 
+ session_id int NOT NULL, location varchar, user_agent varchar,
+ CONSTRAINT songplayuser 
+ UNIQUE(start_time, user_id, level, session_id));
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS users (user_id varchar, first_name varchar, last_name varchar, gender varchar, level varchar);
+CREATE TABLE IF NOT EXISTS users (user_id varchar PRIMARY KEY, 
+first_name varchar, last_name varchar, gender varchar, 
+level varchar NOT NULL);
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, year int, duration numeric);
+CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY, 
+title varchar NOT NULL, artist_id varchar NOT NULL, year int, 
+duration numeric NOT NULL);
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar, location varchar, latitude numeric, longitude numeric);
+CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY, 
+name varchar NOT NULL, location varchar, latitude numeric, 
+longitude numeric);
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time (start_time timestamp, hour int, day int, week int, month int, year int, week_day varchar);
+CREATE TABLE IF NOT EXISTS time (start_time timestamp UNIQUE NOT NULL, 
+hour int, day int, week int, month int, year int, week_day varchar);
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
- VALUES (%s,%s,%s,%s,%s,%s,%s,%s);
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id,
+ session_id, location, user_agent) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+ ON CONFLICT ON CONSTRAINT songplayuser
+ DO NOTHING;
 """)
 
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level) 
- VALUES (%s,%s,%s,%s,%s);
+ VALUES (%s,%s,%s,%s,%s)
+ ON CONFLICT (user_id) 
+ DO
+ UPDATE
+   SET level = EXCLUDED.level, first_name = EXCLUDED.first_name,
+   last_name = EXCLUDED.last_name, gender = EXCLUDED.gender;
 """)
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration) 
- VALUES (%s,%s,%s,%s,%s);
+ VALUES (%s,%s,%s,%s,%s)
+ ON CONFLICT (song_id) 
+ DO
+ NOTHING;
 """)
 
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, latitude, longitude) 
- VALUES (%s,%s,%s,%s,%s);
+ VALUES (%s,%s,%s,%s,%s)
+ ON CONFLICT (artist_id) 
+ DO
+ NOTHING;
 """)
 
 
 time_table_insert = ("""
 INSERT INTO time (start_time, hour, day, week, month, year, week_day) 
- VALUES (%s,%s,%s,%s,%s,%s,%s);
+ VALUES (%s,%s,%s,%s,%s,%s,%s)
+ ON CONFLICT (start_time)
+ DO
+ NOTHING;
 """)
 
 # FIND SONGS
-# you'll need to get the song ID and artist ID by querying the songs and artists tables
-# to find matches based on song title, artist name, and song duration time
+# you'll need to get the song ID and artist ID by querying the songs
+# and artists tables to find matches based on song title, artist name,
+# and song duration time
 song_select = ("""
 SELECT s.song_id, s.artist_id FROM songs s
  JOIN artists a ON s.artist_id=a.artist_id
@@ -67,5 +96,9 @@ SELECT s.song_id, s.artist_id FROM songs s
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create, user_table_create,
+                        song_table_create, artist_table_create,
+                        time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop,
+                      song_table_drop, artist_table_drop,
+                      time_table_drop]
